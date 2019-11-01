785ed61be01754b4a59b60893194894c8e735aed
Raja Shekar <irajashekarb@gmail.com>
2019-11-01 09:14:03 +0530

    Update PlotTemps.scala

--- src/main/scala/standartscala/PlotTemps.scala
+++ src/main/scala/standartscala/PlotTemps.scala
@@ -11,7 +11,8 @@ object PlotTemps extends JFXApp {
   val lines = source.getLines().drop(1)
   val data = lines.flatMap { line =>
     val p = line.split(",")
-    if(p(7)=="." || p(8)=="." || p(9)==".") Seq.empty else
+    if(p(7)=="." || p(8)=="." || p(9)==".") Seq.empty
+    else
       Seq(TempData(p(0).toInt, p(1).toInt, p(2).toInt, p(4).toInt,
         TempData.toDoubleOrNeg(p(5)), TempData.toDoubleOrNeg(p(6)), p(7).toDouble, p(8).toDouble,
         p(9).toDouble))
