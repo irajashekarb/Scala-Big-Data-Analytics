7c0d888572335f136f63e763f855717aa1a9da5f
Raja Shekar <irajashekarb@gmail.com>
2019-11-02 13:45:02 +0530

    update

--- src/main/scala/sparkrdd/RDDTempData.scala
+++ src/main/scala/sparkrdd/RDDTempData.scala
@@ -1,17 +1,29 @@
 package sparkrdd
 
 import org.apache.spark.{SparkConf, SparkContext}
+import standartscala.TempData
 
 object RDDTempData {
-  def main(args: Array[String]): Unit = {
+  def main(args : Array[String]): Unit = {
     //Creating configuration for our spark context
     val conf = new SparkConf().setAppName("Temp Data").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     //Loading lines using textFile method
-    val lines = sc.textFile("MN212142_9392.csv")
+    val lines = sc.textFile("MN212142_9392.csv").filter(!_.contains("Day"))
 
+    val data = lines.flatMap
+    {
+      line =>
+      val p = line.split(",")
+      if(p(7)=="." || p(8)=="." || p(9)==".") Seq.empty
+      else
+        Seq(TempData(p(0).toInt, p(1).toInt, p(2).toInt, p(4).toInt,
+          TempData.toDoubleOrNeg(p(5)), TempData.toDoubleOrNeg(p(6)), p(7).toDouble, p(8).toDouble,
+          p(9).toDouble))
+    }
     //printing first five lines from our file
-    lines.take(5) foreach(println)
+    data.take(5) foreach(println)
+    println(data.count())
   }
 }
