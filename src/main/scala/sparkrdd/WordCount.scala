f55542871c54829c620b6f6b57831ebb486d473d
Raja Shekar <irajashekarb@gmail.com>
2019-11-06 17:21:21 +0530

    update

--- src/main/scala/sparkrdd/WordCount.scala
+++ src/main/scala/sparkrdd/WordCount.scala
@@ -4,12 +4,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
   def main(args: Array[String]):Unit = {
-    val conf = new SparkConf().setAppName("Spark Count App").setMaster("Local[*]")
+    val conf = new SparkConf().setAppName("Spark Count App").setMaster("local[*]")
     val sc = new SparkContext(conf)
-
+    sc.setLogLevel("WARN")
     val inputFileName = "InputFile.txt"
-    val outputFileName = "/out.txt"
+    val outputFileName = "Scala-Big-Data-Analytics/out.txt"
 
-    sc.textFile()
+    sc.textFile(inputFileName)
+      .flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+      .saveAsTextFile(outputFileName)
   }
 }
