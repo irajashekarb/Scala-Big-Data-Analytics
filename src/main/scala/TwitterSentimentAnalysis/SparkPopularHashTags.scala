91a9ba0eda423715eaaa95542207104f6ce8ca60
Raja Shekar <irajashekarb@gmail.com>
2019-11-23 21:52:22 +0530

    update

--- src/main/scala/TwitterSentimentAnalysis/SparkPopularHashTags.scala
+++ src/main/scala/TwitterSentimentAnalysis/SparkPopularHashTags.scala
@@ -1,9 +1,8 @@
 package TwitterSentimentAnalysis
 
+import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.streaming.twitter4j._
-
 object SparkPopularHashTags {
   val conf = new SparkConf().setMaster("local[2]").setAppName("Spark Streaming - Popular Hashtags")
   val sc = new SparkContext(conf)
@@ -21,6 +20,30 @@ object SparkPopularHashTags {
 
     val ssc = new StreamingContext(sc, Seconds(5))
 
-    val stream = new TwitterUtils
+    val stream = TwitterUtils.createStream(ssc, None, filters)
+
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60)).map { case (topic, count) => (count, topic) }.transform(_.sortByKey(false))
+
+    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+      .map { case (topic, count) => (count, topic) }
+      .transform(_.sortByKey(false))
+
+    stream.print()
+
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+    topCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
