dc4cd549d3393b94d1e5de219516e7d7ff3f081e
Raja Shekar <irajashekarb@gmail.com>
2019-11-23 20:35:30 +0530

    Update SparkPopularHashTags.scala

--- src/main/scala/TwitterSentimentAnalysis/SparkPopularHashTags.scala
+++ src/main/scala/TwitterSentimentAnalysis/SparkPopularHashTags.scala
@@ -1,5 +1,17 @@
 package TwitterSentimentAnalysis
 
-object SparkPopularHashTags {
+import org.apache.spark.{SparkConf, SparkContext}
 
+object SparkPopularHashTags {
+  val conf = new SparkConf().setMaster("local[2]").setAppName("Spark Streaming - Popular Hashtags")
+  val sc = new SparkContext(conf)
+
+  def main(args: Array[String]): Unit = {
+    sc.setLogLevel("WARN")
+
+    val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
+    val filters = args.takeRight(args.length - 4)
+
+    
+  }
 }
